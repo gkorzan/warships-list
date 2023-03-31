f736279106cd5c6f9d9a2dbd2449d7588adc0407
Gleb Korzan <gleb.korzan@gmail.com>
2023-03-31 20:10:17 +0300

    feat: initial commit w/ working react server w/o create-react-app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Warship List
+
+WIP Project which main goal is to showcase all of the World of Warships vehicles in their glory
---
+++ src/bff/api-constants.ts
@@ -0,0 +1,6 @@
+const VECHILES_URL = 'https://vortex.worldofwarships.eu/api/encyclopedia/en/vehicles/' as const
+const NATIONS_URL = 'https://vortex.worldofwarships.eu/api/encyclopedia/en/nations/' as const
+const VECHILE_TYPES_COMMON_URL = 'https://vortex.worldofwarships.eu/api/encyclopedia/en/vehicle_types_common/' as const
+const MEDIA_PATH_URL = 'https://vortex.worldofwarships.eu/api/encyclopedia/en/media_path/' as const
+
+export {VECHILES_URL, NATIONS_URL, VECHILE_TYPES_COMMON_URL, MEDIA_PATH_URL}
