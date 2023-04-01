e5151035e7dbf67da6093be19b5f6a386488931b
Gleb Korzan <gleb.korzan@gmail.com>
2023-04-01 16:39:30 +0300

    feat: added dotenv plugin and constants

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   mode: prod ? 'production' : 'development',
@@ -30,6 +31,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.html'
     }),
-    new MiniCssExtractPlugin()
-  ]
+    new MiniCssExtractPlugin(),
+    new Dotenv({
+      path: './.env',
+      safe: true, // load .env.example (defaults to "false" which does not use dotenv-safe)
+    })
+  ],
 };
